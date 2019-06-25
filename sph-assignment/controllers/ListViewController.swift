2f0dd6e8d0c7244504959ddc5fa66fad4fdd5fa2
Kelvin Fok <punkd1@gmail.com>
2019-06-25 14:31:58 +0800

    Initial Commit

---
+++ sph-assignment/controllers/ListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sph-assignment
+//
+//  Created by Kelvin Fok on 25/6/19.
+//  Copyright © 2019 Kelvin Fok. All rights reserved.
+//
+
+import UIKit
+
+class ListsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
